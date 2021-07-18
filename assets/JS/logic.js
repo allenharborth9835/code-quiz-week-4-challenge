cec65f4374ad8e8e1b769d65da206adbc6f71e49
Harborth, Allen <allenharborth9835@gmail.com>
2021-07-17 20:01:55 -0500

    add style logic and html

--- assets/JS/logic.js
+++ assets/JS/logic.js
@@ -1,35 +1,35 @@
-
-var startBtn = document.createElement("BUTTON");
-startBtn.innerHTML = "start game";
-var headerEl = document.createElement("H1");
-headerEl.innerHTML = "take the code quizz";
-var phraseEl = document.createElement("P");
-phraseEl.innerHTML = "find out how you compare to your freinds"
-var mainDivEl = document.createElement("div")
-var score = 0;
-let time = 45;
+let startEl = document.querySelector('#start');
+let mainDivEl = document.querySelector('#game');
+let highscoreEl = document.querySelector('#highscore');
+let highscoreInputEl = document.querySelector('#highscoreInput');
+let responseEl = document.querySelector("#response");
 let timeEl = document.getElementById('time');
+let scoreArr = [];
+let score = 0;
+let time = 45;
+
+
 
 //create a function to create the start page
 function startPage(){
-    document.body.appendChild(mainDivEl)
-    mainDivEl.appendChild(headerEl);
-    mainDivEl.appendChild(phraseEl);
-    mainDivEl.appendChild(startBtn);
+    highscoreInputEl.remove();
+    highscoreEl.remove();
+    highscoreEl.remove();
+    var localHighscores = localStorage.getItem("highscore");
+    if(!(localHighscores===null)){
+        localHighscores = JSON.parse(localHighscores);
+        localHighscores.forEach(score =>{
+            scoreArr.push(score)
+        });
+    }
+    let startBtn = document.querySelector('#startGame')
     startBtn.onclick = startGame;
 }
-function submitHighscore(event){
-    event.preventDefault();
-    highscoreReview();
-}
-
 //create function to create game
 function startGame(){
-    var i = 0;
-    var questionEl = document.createElement("H3");
-    var responseEl = document.createElement("P");
-    var responseDiv = document.createElement("footer");
-    var buttonElArr =[
+    let i = 0;
+    let questionEl = document.createElement("H1");
+    let buttonElArr =[
         document.createElement("BUTTON"),
         document.createElement("BUTTON"),
         document.createElement("BUTTON"),
@@ -38,7 +38,7 @@ function startGame(){
     //create a varible to store
     score = 0;
     //create a varible to store questions
-    var questions =[
+    let questions =[
         {question: "1",
         answers:[ 
             "1.",
@@ -60,32 +60,28 @@ function startGame(){
         isAnswered: false
         }
     ]
-    headerEl.remove();
-    phraseEl.remove();
-    startBtn.remove();
-
+    startEl.remove();
+    highscoreEl.remove();
     document.body.appendChild(mainDivEl);
-    document.body.appendChild(responseDiv);
+    document.body.appendChild(responseEl);
 
     function displayNext(){   
         if((i<questions.length)&&(!questions[i].isAnswered)){
             questionEl.innerHTML = questions[i].question;
             mainDivEl.appendChild(questionEl);
-            var answerArr = questions[i].answers;
+            let answerArr = questions[i].answers;
             answerArr.forEach( answer =>{
-                var position = answerArr.indexOf(answer);
+                let position = answerArr.indexOf(answer);
                 buttonElArr[position].innerHTML = answer;
                 mainDivEl.appendChild(buttonElArr[position]);
                 buttonElArr[position].onclick = function(){
                     if(position === questions[i].correct){
                         time += 5;
                         score++;
-                        responseEl.innerHTML = "correct! +5 time";
-                        responseDiv.appendChild(responseEl); 
+                        responseEl.innerHTML = "correct! +5 time"; 
                     }else{
                         time -= 10;
-                        responseEl.innerHTML = "incorrect! -10 time";
-                        responseDiv.appendChild(responseEl);
+                        responseEl.innerHTML = "incorrect! -10 time"
                     }
                     questions[i].isAnswered = true;
                     i++;
@@ -98,7 +94,7 @@ function startGame(){
     }
     displayNext();
     //create a varible for time
-    var timerInterval = setInterval(function(){
+    let timerInterval = setInterval(function(){
         if(time > -1){
             timeEl.innerHTML = time;
             time--;
@@ -107,7 +103,6 @@ function startGame(){
             for(var i = 0; i < 4;i++){
                 buttonElArr[i].remove();
             }
-            mainDivEl.remove();
             clearInterval(timerInterval);
             time=45;
             timeEl.innerHTML = time;
@@ -115,41 +110,63 @@ function startGame(){
         }
     }, 1000);
 }
-
 //create a highscore input page
 function highscoreInput(){
-    var scoreEl = document.createElement("P");
-    scoreEl.innerHTML = `your highscore was ${score}`;
-    var promptEl = document.createElement("P");
-    var formEl = document.createElement("FORM")
-    promptEl.innerHTML = "enter your intials"
-    var inputEl = document.createElement('input');
-    inputEl.setAttribute("type","text");
-    inputEl.setAttribute("maxLength", "3");
-    var submitEl = document.createElement("BUTTON");
-    submitEl.innerHTML = "submit";
+    responseEl.remove();
+    mainDivEl.remove();
+    document.body.appendChild(highscoreInputEl)
+    document.body.appendChild(responseEl)
 
-    document.body.appendChild(mainDivEl);
-    document.body.appendChild(formEl);
-
-    mainDivEl.appendChild(scoreEl);
-    mainDivEl.appendChild(formEl);
-    formEl.appendChild(promptEl);
-    formEl.appendChild(inputEl);
-    formEl.appendChild(submitEl);
+    let scoreEl = document.getElementById('score');
+    let submitEl = document.querySelector('#submitBtn')
+    scoreEl.innerHTML = score;
+    
     submitEl.onclick = submitHighscore;
 }
+function submitHighscore(event){
+    event.preventDefault();
+    var NameInput = document.querySelector("input[id='name']").value;
+    let scoreInfo = [];
+    scoreInfo.push(NameInput);
+    scoreInfo.push(score);
+    console.log(scoreInfo)
+    scoreArr.push(scoreInfo);
+    localStorage.setItem("highscore",JSON.stringify(scoreArr));
+    
+    highscoreReview();
+}
+
 //create a highscore page
 function highscoreReview(){
-    var highscore = document.createElement("div");
-    var header = document.createElement("H1");
-    header.innerHTML = "Highscores"
+    
+    if(scoreArr >  2){
+        
+    }
+    responseEl.remove()
+    startEl.remove();
+    highscoreInputEl.remove();
     mainDivEl.remove();
-    document.body.appendChild(highscore);
-    highscore.appendChild(header);
-
+    document.body.appendChild(highscoreEl);
+    let scoresEl = document.querySelector("#scores")
+    scoresEl.innerHTML = "";
+    console.log(scoreArr);
+    scoreArr.forEach( score =>{
+        let postition = scoreArr.indexOf(score);
+        let number = `${postition + 1}. ${score[0]} ${score[1]}<br>`;
+        scoresEl.innerHTML += number;
+    });
+    highscoreEl.appendChild(scoresEl);
+    let restartBtn = document.querySelector('#restart');
+    let clearBtn = document.querySelector('#clear')
+    restartBtn.onclick = startGame;
+    clearBtn.onclick = function(){
+        localStorage.removeItem("highscore");
+        scoreArr = [];
+        scoresEl.innerHTML = "";
+    };
 }
 //create function to store data
 //create funtion to load data
+//create a function to clear data
 startPage();
 
